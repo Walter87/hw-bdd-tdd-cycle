9317dcbc0ca1baa616696d626ebba91329c17f50
Grzegorz <gb87@wp.pl>
2015-12-01 21:26:40 +0100

    Solve assignment 2.1

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -15,7 +15,10 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
-
+    when /^the edit page for "(.+)"/
+      title = $1
+      movie =  Movie.find_by_title title
+      edit_movie_path movie.id
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
